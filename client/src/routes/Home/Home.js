83174f9e22a35a0ef8c3de856588662ffe7d6c67
glowing713 <glowing713@naver.com>
2021-06-09 17:49:49 +0900

    feat: Edit hover text in About us

--- client/src/routes/Home/Home.js
+++ client/src/routes/Home/Home.js
@@ -17,9 +17,15 @@ function Home() {
           <div className="menu__about">
             <div className="title ft_white">About us</div>
             <div className="box_info">
-              <em className="category ft_white">Baepo는?</em>
+              <em className="category ft_white ft_bold">Team Baepo는?</em>
               <p className="hover_text">
-                boostcamp AI Tech 1기 캠퍼들이 만든 DKT task 체험 공간입니다.
+                저희는 boostcamp AI Tech 1기
+                <br />
+                Team Baepo 입니다🔥
+                <br />P stage 4 DKT task를 진행하면서 서비스화 한다는
+                마음가짐으로 프로젝트를 진행했습니다.
+                <br />
+                프로젝트의 상세한 내용은 파트별로 나누어 담아보았습니다🙌🏼
               </p>
             </div>
           </div>
