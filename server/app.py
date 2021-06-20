add51009f452a25af34080bdd4ba49a9f4f1dc8f
glowing713 <glowing713@naver.com>
2021-06-21 03:27:11 +0900

    fix: Changed cors solution CORS package to Adding Access-Control-Allow-Origin header

--- server/app.py
+++ server/app.py
@@ -7,11 +7,11 @@ from flask_restx import Resource, Api
 from todo import Todo
 from inference import Inference
 from analysis import Analysis
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 app = Flask(__name__, static_url_path='/static')
-CORS(app)
+# CORS(app)
 api = Api(
     app,
     version='0.1',
