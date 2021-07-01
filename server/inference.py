90748d0d87b263fdda18f2578a766ae9bd3b07c7
glowing713 <glowing713@naver.com>
2021-07-01 16:16:21 +0900

    Change adding CORS header method

--- server/inference.py
+++ server/inference.py
@@ -31,17 +31,22 @@ class TodoPost(Resource):
     @Inference.response(201, 'Success', inference_fields_with_data)
     @Inference.response(500, 'Failed')
     def get(self):
+        response_header = dict()
+        allowed_origins = ['http://localhost:3000', 'https://baepo.netlify.app']
         """Inference 반환."""
         df = pd.read_csv('./upload/test.csv')
         data_path = 'data_file'
         raw, score = make_result(data_path, df, True) # 0. plot_importance, 0 & 1 pred histogram
         
         print('결과', raw['pred'].iloc[-1])
-        
+        response_header['Access-Control-Allow-Methods'] = 'GET'
+        if request.headers['Origin'] in allowed_origins:
+            response_header['Access-Control-Allow-Origin'] = request.headers['Origin']
+
         return {
             'prediction': raw['pred'].iloc[-1],
             'accuracy_score' : score['acc'],
             'roc_auc_score' : score['roc'],
             'lgbm_plot_importance' : "_00_lgbm_plot_importance.png",
             'zero_one_distribution': "_01_zero_one_distribution.png",
-        }, 201, {'Access-Control-Allow-Origin': 'http://localhost:3000, https://baepo.netlify.app', 'Access-Control-Allow-Methods': 'GET'}
+        }, 201, response_header
