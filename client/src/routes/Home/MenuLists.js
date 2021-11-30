7f36be6ff2ae8789c563d93ea5ed3b043676d72d
glowing713 <glowing713@naver.com>
2021-11-30 22:55:48 +0900

    chore: Restore menu items

--- client/src/routes/Home/MenuLists.js
+++ client/src/routes/Home/MenuLists.js
@@ -1,23 +1,23 @@
 // 메뉴로 구성될 항목들을 모아둔 배열입니다.
 const MenuLists = [
-  // {
-  //   name: "crews",
-  //   category: "Introduction",
-  //   title: "Crew Intro",
-  //   sub_info: "@Baepo crews",
-  // },
-  // {
-  //   name: "task",
-  //   category: "Overview",
-  //   title: "Task Overview",
-  //   sub_info: "@Baepo crews",
-  // },
-  // {
-  //   name: "eda",
-  //   category: "Pre-stage",
-  //   title: "Data EDA",
-  //   sub_info: "@someone",
-  // },
+  {
+    name: "crews",
+    category: "Introduction",
+    title: "Crew Intro",
+    sub_info: "@Baepo crews",
+  },
+  {
+    name: "task",
+    category: "Overview",
+    title: "Task Overview",
+    sub_info: "@Baepo crews",
+  },
+  {
+    name: "eda",
+    category: "Pre-stage",
+    title: "Data EDA",
+    sub_info: "@someone",
+  },
   {
     name: "model",
     category: "Modeling",
