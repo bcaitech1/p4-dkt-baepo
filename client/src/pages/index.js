f2cd2cb5073e45b8880658cf94f666c7df7cd7fa
glowing713 <glowing713@naver.com>
2021-12-01 18:27:32 +0900

    chore: Restore Analysis component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "../routes/Home";
 export { default as Task } from "../routes/Task";
 export { default as Eda } from "../routes/Eda";
 export { default as Model } from "../routes/Model";
+export { default as Analysis } from "../routes/Analysis";
