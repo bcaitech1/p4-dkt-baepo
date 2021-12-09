ed041bf51996c7eb4ad363987cbf213f0ff5d342
glowing713 <glowing713@naver.com>
2021-12-09 21:44:37 +0900

    feat: Add mobile view navbar Added hamburger bar and checked menu open status with expand state.

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 const Navigation = () => {
-  const [expand, setExpand] = useState(false);
-  const menuButton = document.getElementsByClassName("menu__button")[0]; // 햄버거바
-  const navBarItems = document.getElementsByClassName(
-    "nav__item__container"
-  )[0]; // 메뉴 아이템 링크
+  const [expand, setExpand] = useState(false); // 모바일뷰 메뉴 상태(expanded, not expanded)
 
   // 스크롤바를 조금이라도 내리게 되면 navbar의 bottom border에 색을 입히는 효과가 추가됨.
   window.addEventListener("scroll", (e) => {
@@ -19,41 +15,66 @@ const Navigation = () => {
     }
   });
 
-  // 햄버거바 메뉴버튼 클릭이벤트
-  // menuButton[0].addEventListener("click", () => {
-  //   menuButton.classList.toggle("active");
-  // });
+  console.log(expand);
 
   return (
     <nav className="nav">
       <div className="nav__container">
         <div className="nav__home__container">
-          <Link to="/" className="nav__home">
+          <Link
+            to="/"
+            className="nav__home"
+            onClick={() => {
+              setExpand(false);
+            }}
+          >
             Baepo
           </Link>
         </div>
         <div className="nav__on__mobile__container">
           <button className="menu__button" onClick={() => setExpand(!expand)}>
             {expand ? (
-              <i className="fas fa-times" />
+              <i className="close-button fas fa-times" />
             ) : (
-              <i className="fas fa-bars" />
+              <i className="open-button fas fa-bars" />
             )}
           </button>
         </div>
-        <div className="nav__item__container">
+        <div
+          className={
+            expand ? "nav__item__container active" : "nav__item__container"
+          }
+        >
           <div className="nav__item">
-            <Link to="/task" className="nav__task">
+            <Link
+              to="/task"
+              className="nav__task"
+              onClick={() => {
+                setExpand(false);
+              }}
+            >
               About DKT
             </Link>
           </div>
           <div className="nav__item">
-            <Link to="/eda" className="nav__eda">
+            <Link
+              to="/eda"
+              className="nav__eda"
+              onClick={() => {
+                setExpand(false);
+              }}
+            >
               Data EDA
             </Link>
           </div>
           <div className="nav__item">
-            <Link to="/model" className="nav__model">
+            <Link
+              to="/model"
+              className="nav__model"
+              onClick={() => {
+                setExpand(false);
+              }}
+            >
               Model Analysis
             </Link>
           </div>
@@ -62,6 +83,9 @@ const Navigation = () => {
               href="https://boostcamp.connect.or.kr/program_ai.html"
               className="nav__bcaitech"
               target="blank"
+              onClick={() => {
+                setExpand(false);
+              }}
             >
               boostcamp AI Tech
             </a>
